189c9994d8d008024fbf1f448ce16a59ed2357b2
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-03-07 17:11:11 +0100

    STL export: Correct normals and skip children of objects
    Correcting normals doesn't work on all objects, but trying to do that whenever possible.
    Additionally, skipping children of objects. Normally, these are used to describe something, so the parent can get created.
    These should be not relevant to be exported.
    (Fingers crossed...)
    
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

--- CuraBlenderPlugin/BpyScripts/ExportAsStl.py
+++ CuraBlenderPlugin/BpyScripts/ExportAsStl.py
@@ -1,12 +1,71 @@
 import sys
 import bpy
 
+ignorable_object_types = ("EMPTY", "LAMP", "CAMERA")
+
 stl_path = sys.argv[-1]
 
+def deselect_all():
+    # Deselect everything
+    for ob in bpy.context.selected_objects:
+        ob.select = False
+
+scene = bpy.context.scene
+
+# Getting all children of all objects
+children = []
+for obj in scene.objects:
+    for child in obj.children:
+        children.append(child)
+
+# Correcting normals of all objects
+for obj in scene.objects:
+    if obj.type in ignorable_object_types and obj not in children:
+        continue
+
+    print("Processing object {} of type: {}".format(repr(obj.name), obj.type))
+    scene.objects.active = obj
+
+    obj.select = True
+
+    mode_changed = False
+    try:
+        bpy.ops.object.mode_set(mode='EDIT')
+        mode_changed = True
+        # recalculate outside normals
+        bpy.ops.mesh.normals_make_consistent(inside=False)
+    except:
+        pass
+    if mode_changed:
+        # go object mode again
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    scene.update()
+
+    deselect_all()
+
+# Processing all objects
+for obj in scene.objects:
+    if obj.type in ignorable_object_types and obj not in children:
+        continue
+
+    print("Processing object {} of type: {}".format(repr(obj.name), obj.type))
+    scene.objects.active = obj
+
+    if not obj.hide:
+        obj.select = True
+
+# Export everything at once
 bpy.ops.export_mesh.stl(filepath = stl_path,
                         check_existing = False,
-                        #use_mesh_modifiers  = False,
-                        #use_selection=False,
-                        #axis_forward = '-Z',
-                        #axis_up = 'Y',
+                        #use_mesh_modifiers  = True,
+                        use_selection=True,
+                        #axis_forward='Y',
+                        #axis_up='Z',
+                        #filter_glob="*.stl",
+                        global_scale=1.0,
+                        #use_scene_unit=True,
+                        #ascii=False,
+                        #use_mesh_modifiers=True,
+                        #batch_mode='OFF'
                         )
